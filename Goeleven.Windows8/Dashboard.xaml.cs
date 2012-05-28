94c2fff91105140947107da5c20696736c7ed739
Yves Goeleven <yves@goeleven.com>
2012-05-27 22:51:39 -0700

    New design for win 8 client (XAML based)

---
+++ Goeleven.Windows8/Dashboard.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Goeleven.Windows8.Data;
+using Goeleven.Windows8.Pages;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Grouped Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234231
+
+namespace Goeleven.Windows8
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class Dashboard : Goeleven.Windows8.Common.LayoutAwarePage
+    {
+        public Dashboard()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property provides the grouped collection of items to be displayed.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var data = (SampleDataSource)e.Parameter;
+            this.DefaultViewModel["Groups"] = data.ItemGroups;
+            this.DefaultViewModel["Messages"] = data.WallMessages;
+        }
+
+        /// <summary>
+        /// Invoked when a group header is clicked.
+        /// </summary>
+        /// <param name="sender">The Button used as a group header for the selected group.</param>
+        /// <param name="e">Event data that describes how the click was initiated.</param>
+        void Header_Click(object sender, RoutedEventArgs e)
+        {
+            // Determine what group the Button instance represents
+            var group = (sender as FrameworkElement).DataContext;
+
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            this.Frame.Navigate(typeof(GroupDetailPage), group);
+        }
+
+        /// <summary>
+        /// Invoked when an item within a group is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            this.Frame.Navigate(typeof(ItemDetailPage), e.ClickedItem);
+        }
+
+        /// <summary>
+        /// Wait with showing controls until the image is adjusted to the screen size
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackgroundImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            content.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        }
+
+        private void Inbox_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //this.Frame.Navigate(typeof(GroupDetailPage), null);
+        }
+
+        private void wallMessage_Clicked(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void getorganized_Click(object sender, ItemClickEventArgs e)
+        {
+            var grid = (Grid) e.ClickedItem;
+
+            switch (grid.Name) { 
+                case "inbox":
+                    this.Frame.Navigate(typeof(Inbox));
+                    break;
+
+            }
+            
+        }
+
+        private void context_Click(object sender, ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Goeleven.Windows8/LiveTiles.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace Goeleven.Windows8
+{
+    public class LiveTiles
+    {
+        private TileUpdater tileUpdater;
+        private BadgeUpdater badgeUpdater;
+        private ToastNotifier toastNotifier;
+
+        public LiveTiles() {
+            tileUpdater = TileUpdateManager.CreateTileUpdaterForApplication();
+            tileUpdater.EnableNotificationQueue(true);
+
+            badgeUpdater = BadgeUpdateManager.CreateBadgeUpdaterForApplication();
+
+            toastNotifier = ToastNotificationManager.CreateToastNotifier();
+        }
+
+        public void ShowAsLiveTile(string text)
+        {
+            XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWideText01);
+
+            XmlNodeList textElements = tileXml.GetElementsByTagName("text");
+            textElements.Item(0).AppendChild(tileXml.CreateTextNode(text));
+
+            TileNotification tile = new TileNotification(tileXml);
+
+            tileUpdater.Update(tile);
+        }
+
+        public void ShowAsBadge(int number)
+        {
+            var badgeXml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeNumber);
+
+            XmlElement badgeElement = (XmlElement)badgeXml.SelectSingleNode("/badge");
+            badgeElement.SetAttribute("value", number.ToString());
+            
+            var badgeNotification = new BadgeNotification(badgeXml);
+            badgeUpdater.Update(badgeNotification);
+        }
+
+        public void ShowAsToast(string text)
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            stringElements.Item(0).AppendChild(toastXml.CreateTextNode(text));
+
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            toastNotifier.Show(toast);
+        }
+    }
+}
